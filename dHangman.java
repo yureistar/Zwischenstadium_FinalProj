2654a9a69228dcac93e4013947c9014a6fe06c50
Dorothy Ng <dng1@stuy.edu>
2016-01-25 00:37:53 -0500

    changes insta var

--- dHangman.java
+++ dHangman.java
@@ -1,14 +1,10 @@
 public class dHangman { //difficult level
     //instance vars
-    private static String strike1="____ "+"\n|"+"\n|"+"\n|"+"\n|"+"\n|";
-    private static String strike2="____ "+"\n|  |"+"\n|"+"\n|"+"\n|"+"\n|";
-    private static String strike3="____ "+"\n|  |"+"\n|  O"+"\n|"+"\n|"+"\n|";
-    private static String strike4="____ "+"\n|  |"+"\n|  O"+"\n|  |"+"\n|  |"+"\n|";
-    private static String strike5="____ "+"\n|  |"+"\n|  O"+"\n| /|"+"\n|  |"+"\n|";
-    private static String strike6="____ "+"\n|  |"+"\n|  O"+"\n| /|\\"+"\n|  |"+"\n|";
-    private static String strike7="____ "+"\n|  |"+"\n|  O"+"\n| /|\\"+"\n|  |"+"\n| / ";
-    private static String strike8="____ "+"\n|  |"+"\n|  O"+"\n| /|\\"+"\n|  |"+"\n| / \\";
-    private String[] strikes = {" ",strike1,strike2,strike3,strike4,strike5,strike6,strike7,strike8};
+    private static String strike1="____ "+"\n|  |"+"\n|  O"+"\n|"+"\n|"+"\n|";
+    private static String strike2="____ "+"\n|  |"+"\n|  O"+"\n|  |"+"\n|  |"+"\n|";
+    private static String strike3="____ "+"\n|  |"+"\n|  O"+"\n| /|\\"+"\n|  |"+"\n|";
+    private static String strike4="____ "+"\n|  |"+"\n|  O"+"\n| /|\\"+"\n|  |"+"\n| / \\";
+    private String[] strikes = {" ",strike1,strike2,strike3,strike4};
 
     public String getHangman(int num){
 	return strikes[num];
